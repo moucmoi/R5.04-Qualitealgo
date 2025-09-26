31c28ef3361af218ed19e3f9e0eafe242d84d101
Fonteny Noa <noatoimoi@gmail.com>
2025-09-26 11:06:57 +0200

    révision

--- qualite_algo/liste/liste.h
+++ qualite_algo/liste/liste.h
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <stdio.h>
 #ifndef LISTE_H
 #define LISTE_H
 
@@ -57,6 +58,7 @@ void supprimerListe(lst l){
 
 
 void ajouterEnTete(lst l, int x){
+    /** 
     int nb_elem=l->nbElem;
     maillon * maaaa=l->debut;
     lst list=creerListe();
@@ -65,12 +67,19 @@ void ajouterEnTete(lst l, int x){
     m.suivant=maaaa;
     list->debut=m;
     list->nbElem=nb_elem+1;
+    */
+   ml Nouvel_elem=(ml) malloc (sizeof(struct maillon));
+   Nouvel_elem->val=x;
+   Nouvel_elem->suivant=l->debut;
+   l->debut=Nouvel_elem;
+   l->nbElem++;
 };
 
 
 
 void inserer(lst l, int val, int i){
-    if(int i==0){
+    /**
+    if( i==0){
         ajouterEnTete(l,val);
     }
     else{
@@ -83,27 +92,81 @@ void inserer(lst l, int val, int i){
             }
             mail=maillon.suivant;
         }
-        ml nouveau;
+        ml nouveau=(ml) malloc (sizeof(struct maillon));
         nouveau.val=val;
         nouveau.suivant=mail;
         prec.suivant=nouveau;
+        l->nbElem+=1;
+    }
+     */
+    if(i==0){
+        ajouterEnTete(l,val);
+    }
+    else{
+        int j=0;
+        ml inter=l->debut;
+        while (j< i-1){
+            iter=inter->suivant;
+            j++;
+        }
+        ml elem=(ml) malloc (sizeof(struct maillon));
+        elem->val=val;
+        elem->suivant=iter.suivant;
+        iter.suivant=elem;
+        l->nbElem++;
     }
 };
 
 
 
 int supprimerEnTete(lst l){
-    lst nouvelle_liste;
-    nouvelle_liste->debut=l->debut->suivant;
-    nouvelle_liste->nbElem=l->nbElem-1;
-    free(l->debut);
-    free(l);
+    ml *tete = l->debut;            // sauvegarder l'ancien premier
+    l->debut = tete->suivant;       // avancer le début
+    free(tete);                     // libérer l'ancien premier
+    l->nbElem--;                    // décrémenter le compteur
+    return 0;
 };
 
 
 int supprimer(lst l, int i){
-    
+    if (i == 0) {
+        return supprimerEnTete(l);
+    }
+
+    // aller jusqu'au précédent (i-1)
+    ml *prec = l->debut;
+    for (int k = 0; k < i - 1; ++k) {
+        prec = prec->suivant;       // garanti non NULL grâce au test des bornes
+    }
+
+    ml *cible = prec->suivant;      // noeud à supprimer
+    prec->suivant = cible->suivant; // le “bypasser”
+    free(cible);                    // libérer la mémoire
+    l->nbElem--;
+    return 0;
+
 };
-void afficherListe(lst l);
+
+
+
+void afficherListe(lst l){
+    /**
+    char res = (char) l->debut->val;
+    maillon * suivant=l->debut;
+    for(int i=0;i<l->nbElem;i++){
+        res = strcat(res,' ---> ');
+        res = strcat(res,suivant->val);
+        suivant=suivant->suivant;
+    }
+    printf(%s,res);
+    */
+   if(l->nbElem>0){
+    ml iter=l->debut;
+    while (iter!=NULL){
+        printf("%d\n",iter.val);
+        iter=iter->suivant;
+    }
+   }
+};
 
 #endif
